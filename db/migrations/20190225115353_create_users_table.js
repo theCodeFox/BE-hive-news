b67080339b2056844abd73e6c92b47a0b86ff6c7
theCodeFox <akkitta@live.co.uk>
2019-02-25 13:03:14 +0000

    notNullable bug fixed - syntax all correct

--- db/migrations/20190225115353_create_users_table.js
+++ db/migrations/20190225115353_create_users_table.js
@@ -3,7 +3,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('users', (usersTable) => {
     usersTable.string('username', 50).primary();
     usersTable.string('avatar_url');
-    usersTable.string('name', 100).notNUllable();
+    usersTable.string('name', 100).notNullable();
   });
 };
 
