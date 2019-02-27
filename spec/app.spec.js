66246bb4611ffb71fd1a18371f9bbc03be410c2e
theCodeFox <akkitta@live.co.uk>
2019-02-27 10:00:04 +0000

    now updates votes if given negative value - TDDd

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -130,7 +130,7 @@ describe('/', () => {
           .then((res) => {
             expect(res.body.article[0].article_id).to.equal(1);
           }));
-        it('PATCH:200 updates votes using obj with int_votes key', () => {
+        it('PATCH:200 updates positive votes using obj with int_votes key', () => {
           const updateVotes = { int_votes: 1 };
           return request
             .patch('/api/articles/1')
@@ -138,6 +138,26 @@ describe('/', () => {
             .expect(200)
             .then((res) => {
               expect(res.body.article[0].votes).to.equal(101);
+            });
+        });
+        it('PATCH:200 updates negative votes using obj with int_votes key', () => {
+          const updateVotes = { int_votes: -1 };
+          return request
+            .patch('/api/articles/1')
+            .send(updateVotes)
+            .expect(200)
+            .then((res) => {
+              expect(res.body.article[0].votes).to.equal(99);
+            });
+        });
+        it('PATCH:200 updates negative votes using obj with int_votes key if votes go below 0', () => {
+          const updateVotes = { int_votes: -101 };
+          return request
+            .patch('/api/articles/1')
+            .send(updateVotes)
+            .expect(200)
+            .then((res) => {
+              expect(res.body.article[0].votes).to.equal(-1);
             });
         });
       });
