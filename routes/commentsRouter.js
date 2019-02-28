252633c00fc4fd41703a04061c675319a98614cb
theCodeFox <akkitta@live.co.uk>
2019-02-28 10:06:16 +0000

    removed unnecessary error handling in comments router

--- routes/commentsRouter.js
+++ routes/commentsRouter.js
@@ -2,9 +2,6 @@ const commentsRouter = require('express').Router();
 const { patchComment, deleteComment } = require('../controllers/comments.js');
 const { handle405 } = require('../db/utils/errors.js');
 
-commentsRouter.route('/')
-  .all(handle405);
-
 commentsRouter.route('/:comment_id')
   .patch(patchComment)
   .delete(deleteComment)
