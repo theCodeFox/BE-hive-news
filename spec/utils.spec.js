76ad835ec636589df2b830d4ae28e80f86601654
theCodeFox <akkitta@live.co.uk>
2019-02-28 10:31:31 +0000

    utils.spec updated to reflex changes in functions

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -10,10 +10,6 @@ describe('createArticleRef', () => {
     expect(createArticleRef([{}])).to.be.an('object');
   });
   it('returns object with key of article title', () => {
-    const input = [{ title: 'a', article_id: 'b' }];
-    expect(createArticleRef(input)).to.eql({ a: 'b' });
-  });
-  it('returns object with key of article title and value of article_id', () => {
     const input = [{ title: 'a', article_id: 1 }];
     expect(createArticleRef(input)).to.eql({ a: 1 });
   });
@@ -25,38 +21,33 @@ describe('createArticleRef', () => {
 
 describe('formatArticles', () => {
   it('returns an array', () => {
-    expect(formatArticles([], [], [])).to.be.an('array');
+    expect(formatArticles([])).to.be.an('array');
   });
   it('returns object with keys title, topic, author, body, created_at, votes, article_id', () => {
     const input1 = [{
       title: 'a', topic: 'b', author: 'c', body: 'd', created_at: 'e', votes: 'f', article_id: 1,
     }];
-    const input2 = [{ author: 'a', article_id: 1 }];
-    const input3 = [{ slug: 'a', article_id: 1 }];
-    expect(formatArticles(input1, input2, input3)[0]).to.have.all.keys('title', 'topic', 'author', 'body', 'created_at', 'votes', 'article_id');
+    expect(formatArticles(input1)[0]).to.have.all.keys('title', 'topic', 'author', 'body', 'created_at', 'votes', 'article_id');
   });
   it('created_at value is an instance of date/time', () => {
     const input1 = [{
       title: 'a', topic: 'b', author: 'c', body: 'd', created_at: 1542284514171, votes: 'e', article_id: 1,
     }];
-    const input2 = { author: 'a', article_id: 1 };
-    const input3 = { slug: 'a', article_id: 1 };
-    expect(formatArticles(input1, input2, input3)[0].created_at).to.be.instanceOf(Date);
+    expect(formatArticles(input1)[0].created_at).to.be.instanceOf(Date);
   });
 });
 
 describe('formatComments', () => {
   it('GET:200 returns array', () => {
-    expect(formatComments([], [], [])).to.be.an('array');
+    expect(formatComments([], [])).to.be.an('array');
   });
   it('GET:200 returns array with keys comment_id, author, article_id, votes, created_at, body', () => {
     const input1 = [{
       comment_id: 1, author: 2, article_id: 3, votes: 4, created_at: 5, body: 6, belong_to: 1,
     }];
     const input2 = [{ 1: 'a' }];
-    const input3 = [{ a: 1 }];
-    expect(formatComments(input1, input2, input3)).to.be.an('array');
-    expect(formatComments(input1, input2, input3)[0]).to.have.all.keys(
+    expect(formatComments(input1, input2)).to.be.an('array');
+    expect(formatComments(input1, input2)[0]).to.have.all.keys(
       'comment_id',
       'author',
       'article_id',
@@ -64,6 +55,6 @@ describe('formatComments', () => {
       'created_at',
       'body',
     );
-    expect(formatComments(input1, input2, input3)[0].created_at).to.be.an.instanceOf(Date);
+    expect(formatComments(input1, input2)[0].created_at).to.be.an.instanceOf(Date);
   });
 });
