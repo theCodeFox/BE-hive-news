99fa3d52beb5496c53e63f2db38daec1df628d1f
theCodeFox <akkitta@live.co.uk>
2019-02-28 10:08:41 +0000

    outerjoin in model unnecessart, changed to leftjoin

--- models/articles.js
+++ models/articles.js
@@ -3,7 +3,7 @@ const connection = require('../db/connection.js');
 exports.fetchArticles = (sort_by, order, limit) => connection('articles')
   .select('articles.author', 'articles.title', 'articles.article_id', 'articles.topic', 'articles.created_at', 'articles.votes')
   .count({ comment_count: 'comment_id' })
-  .fullOuterJoin('comments', 'articles.article_id', 'comments.article_id')
+  .leftJoin('comments', 'articles.article_id', 'comments.article_id')
   .groupBy('articles.article_id')
   .orderBy(sort_by, order)
   .limit(limit);
